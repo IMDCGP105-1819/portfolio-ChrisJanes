41f613937b572a5d36134c07ab50c81b920cd5cd
Chris Janes <gingoreilly@gmail.com>
2019-08-08 15:49:24 +0100

    added test for not being able to save enough

--- test_ex9.py
+++ test_ex9.py
@@ -2,3 +2,6 @@ from ex9 import amount_to_save
 
 def test_bisection_percentage():
     assert amount_to_save(100000) == (True, 0.66, 17)
+
+def test_bisection_percentage_2():
+    assert amount_to_save(10000) == (False, 0, 0)
